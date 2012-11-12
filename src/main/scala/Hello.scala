436e2f8454b945898ca2c79e16ece2ba51e81d2f
agiledon <bruce.e.zhang@gmail.com>
2012-11-12 21:08:10 +0800

    Yi - create hello and relate test to make sure scala and scalatest can work

---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 11/12/12
+ * Time: 9:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Hello {
+  def main(args: Array[String]) = println("Hello Scala")
+}
---
+++ src/test/scala/HelloTest.scala
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 11/12/12
+ * Time: 9:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class HelloTest extends org.scalatest.Suite {
+  def testOk() {
+    assert(true)
+  }
+
+}
