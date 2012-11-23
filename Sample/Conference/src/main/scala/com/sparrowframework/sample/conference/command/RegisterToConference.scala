1d4b5d99bb6f2ffd03d197d731240e286676bb69
agiledon <bruce.e.zhang@gmail.com>
2012-11-23 19:56:13 +0800

    Yi - add the sample module for sparrow framework

---
+++ Sample/Conference/src/main/scala/com/sparrowframework/sample/conference/command/RegisterToConference.scala
@@ -0,0 +1,12 @@
+package com.sparrowframework.sample.conference.command
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhang Yi
+ * Date: 11/23/12
+ * Time: 7:53 PM
+ */
+class RegisterToConference{
+
+}
---
+++ Sample/Conference/src/test/scala/com/sparrowframework/sample/conference/command/RegisterToConferenceTest.scala
@@ -0,0 +1,11 @@
+package com.sparrowframework.sample.conference.command
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhang Yi
+ * Date: 11/23/12
+ * Time: 7:55 PM
+ */
+class RegisterToConferenceTest{
+
+}
